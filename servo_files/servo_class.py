f0540efb065f0c94183e0460852fdee70aaf2eda
tibame201204 <77776911+tibame201204@users.noreply.github.com>
2021-01-27 02:10:31 +0800

    Add files via upload

--- servo_files/servo_class.py
+++ servo_files/servo_class.py
@@ -1,6 +1,23 @@
+import time
+from adafruit_servokit import ServoKit
 import os
 url = os.path.dirname(os.path.abspath(__file__))
 os.chdir(url) 
 
 class MyServo():
-	pass
+	def __init__(self):
+		self.kit = ServoKit(channels=16)
+	
+	def run(self):
+		self.kit.servo[0].angle = 180
+		time.sleep(0.5)
+		self.kit.servo[0].angle = 0
+		time.sleep(0.5)
+		self.kit.servo[0].angle = 180
+		time.sleep(0.5)
+		self.kit.servo[0].angle = 0
+
+
+
+if __name__ == '__main__':
+	MyServo().run()
